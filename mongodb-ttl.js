de6a096912b18573957b5aa779cf03cc5d2b1e54
knoxgon <volkan.guven@hotmail.se>
2019-05-28 11:18:09 +0200

    Complete code

--- mongodb-ttl.js
+++ mongodb-ttl.js
@@ -1,23 +1,45 @@
 const MongoClient = require('mongodb').MongoClient;
-const url = "mongodb://localhost:27017/jwt";
 
-MongoClient.connect(url, function (err, db) {
+//Set your database
+const yourMongoDatabase = "usersJwt"
+//Set your url with database
+const url = "mongodb://localhost:27017/" + yourMongoDatabase;
+
+//Connect to your database
+MongoClient.connect(url, (err, db) => {
     if (err) throw err;
 
-    var dbo = db.db("jwt");
+    //Retrieve your chosen database
+    let dbo = db.db(yourMongoDatabase);
 
-    var document = { createdAt: new Date(), name: "David", title: "About MongoDB" };
-
-    dbo.collection("tokens").insert(document, (err, doc) => {
-        if (!err) console.log('Created');
-    });
-
-    /*dbo.collection("tokens")
-        .createIndex({ "lastModifiedDate": 1 }, { expireAfterSeconds: 30 },
-            function (err, res) {
+    /*  Create a mongodb index to remove any document with 'createdAt' 
+       field every 30 seconds.
+   */
+    dbo.collection("tokens")
+        .createIndex({ "createdAt": 1 }, { expireAfterSeconds: 30 },
+            (err, dbResult) => {
                 if (err) throw err;
                 console.log("Index Created");
                 db.close();
-            });*/
-});
+            });
 
+
+    //Prepare your document
+    let document = {
+        createdAt: new Date(),
+        jwt: "example-jwt_ey720adada8fa7241nas",
+        owner: "ExampleUserName"
+    };
+    /*
+        It is required that you have the field 'createdAt' in your document (table)
+        so the mongodb indexer can go through and remove it based on the TTL condition
+    */
+
+    //Set your collection
+    let myCollection = "tokens";
+
+    //Insert the prepared document
+    dbo.collection(myCollection).insert(document, (err, doc) => {
+        if (!err) console.log('Document inserted');
+    });
+});
